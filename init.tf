7b1ff589b86e768c12618f0a02b21264b4626254
Lotarccc <firstolwb16@gmail.com>
2021-03-31 00:46:52 +0300

    Fast run for testing purposes

--- init.tf
+++ init.tf
@@ -3,52 +3,65 @@ variable "region" {
   description = "This is where you have to mention region"
   default = "ap-northeast-1"
 }
+
 variable "name" {
   type        = string
+  default = ""
   description = "This is the Parent instance type name. \nFor 3 instance it would be like the following,\nIf the name is helios \n1. helios-ucp-leader\n2. helios_instance-0\n3. helios_instance-1"
 }
 variable "caseNo" {
   type        = string
+  default = "test"
   description = "This is the case number to track the issue."
 }
 variable "workerCount" {
   type        = string
+  default = "1"
   description = "Please type the total number of worker"
 }
 variable "managerCount" {
   type        = string
+  default = "0"
   description = "Please type the total number of manager"
 }
 variable "dtrCount" {
   type        = string
+  default = "0"
   description = "Please type the total number of dtr"
 }
 variable "ami" {
   type        = string
+  default = 3
   description = "Please type which AMI you want. \n1. ami-02b658ac34935766f : Ubuntu Server 18.04 LTS (HVM) \n2. ami-0ce107ae7af2e92b5 : Amazon Linux 2 \n3. ami-07dd14faa8a17fb3e : Red Hat Enterprise Linux 8 (HVM)\n4. ami-0119d7d47f3b13adb : SUSE Linux Enterprise Server 15 SP2\n5. ami-09b86f9709b3c33d4 : Ubuntu Server 20.04 LTS (HVM)\n"
 }
 variable "amiUserName" {
   type        = string
+  default = "ec2user"
   description = "This is the AMI username. For ubuntu it's ubuntu, for centos it's centos etc."
 }
 variable "ucpInstanceType" {
   type        = string
+  default = "1"
   description = "Please type which Instance type you want. \n1. c4.xlarge : 4 vCPU - 4G MEM\n2. c4.2xlarge  : 8 vCPU - 15.7 G MEM\n3. m4.xlarge : 4 vCPU - 16 G MEM [ Best For Prod reproduce ]"
 }
 variable "dtrInstanceType" {
   type        = string
+  default = "1"
   description = "Please type which Instance type you want. \n1. c4.xlarge : 4 vCPU - 4G MEM\n2. c4.2xlarge  : 8 vCPU - 15.7 G MEM\n3. m4.xlarge : 4 vCPU - 16 G MEM [ Best For Prod reproduce ]"
 }
 variable "workerInstanceType" {
   type        = string
+  default = "1"
   description = "Please type which Instance type you want. \n1. c4.xlarge : 4 vCPU - 4G MEM\n2. c4.2xlarge  : 8 vCPU - 15.7 G MEM\n3. m4.xlarge : 4 vCPU - 16 G MEM [ Best For Prod reproduce ]"
 }
 variable "managerInstanceType" {
   type        = string
+  default = "1"
   description = "Please type which Instance type you want. \n1. c4.xlarge : 4 vCPU - 4G MEM\n2. c4.2xlarge  : 8 vCPU - 15.7 G MEM\n3. m4.xlarge : 4 vCPU - 16 G MEM [ Best For Prod reproduce ]"
 }
 variable "publicKey" {
   type        = string
+  default = "./key-pair.pub"
   description = "If you are using a customized key, please paste your public key here."
 }
 
@@ -66,10 +79,12 @@ variable "docker_ee_version" {
 }
 variable "docker_ucp_version" {
   type        = string
+  default = "3.3.4"
   description = "Please type your desired docker version"
 }
 variable "docker_dtr_version" {
   type        = string
+  default = "2.8.5"
   description = "Please type your desired DTR version"
 }
 variable "admin_username" {
