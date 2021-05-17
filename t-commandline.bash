9be2ac791dab8a4e502dac9016b10731e94df139
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2021-05-17 13:32:48 +0200

    Update t-commandline.bash

--- t-commandline.bash
+++ t-commandline.bash
@@ -40,8 +40,9 @@ t-destroy-lab() {
 t-destroy-cluster() { 
   echo "t-destroy-cluster was called "
 }  
-# what about t-destroy-instances ?? 
-### t stop managers|msrs|workers|windows   
+# what about t-destroy-instances ?? Will we implement such thing? 
+### t stop managers|msrs|workers|windows   DONE 
+### Have not implemented the : t stop manager1 (t stop manager 1)
 t-stop() {
   case "$1" in
     "managers") t-stop-managers
@@ -56,7 +57,7 @@ t-stop() {
 	   exit ;;
   esac
 }
-t-stop-managers(){ 
+t-stop-managers() { 
   echo "t-stop-managers was called"
 }
 t-stop-msrs() { 
@@ -68,7 +69,23 @@ t-stop-workers() {
 t-stop-windows() { 
   echo "t-stop-windows was called, checking if there are windows nodes, and then stopping them "
 }
-#### t show ip managers|msrs|workers|windows|all  DONE 
+### t gen client-bundle|msr-login|swarm-service|k8s-service|msr-images
+t-gen-client_bundle() {
+  echo "t gen client-bundle was called"
+}
+t-gen-msr_login() {
+  echo "t gen msr-login was called"
+}
+t-gen-swarm_service() {
+  echo "t gen swarm-service was called"
+}
+t-gen-k8s_service() {
+  echo "t gen k8s-service was called"
+}
+t-gen-msr_images() {
+  echo "t gen msr-images was called"
+}
+#### t show ip managers|msrs|workers|windows|all 
 t-show-ip() {
   case "$1" in
     "managers") t-show-ip-managers
@@ -103,7 +120,7 @@ t-show-ip-all() {
   t-show-ip-workers
   # t-show-ip-windows   # if windows VMs exist 
 }
-#### t show dns managers|msrs|workers|windows|all  
+#### t show dns managers|msrs|workers|windows|all   
 t-show-dns() {
   case "$1" in
     "managers") t-show-dns-managers
@@ -165,7 +182,7 @@ t-show-hostname-workers() {
 t-show-hostname-windows() { 
   echo "t show hostname windows was typed"
 }
-#### t show creds mke|msr                          
+#### t show creds mke|msr                           
 t-show-creds-mke() { 
   echo "t show creds mke was typed "
 }
@@ -180,6 +197,21 @@ t-show-access​_ke​y_windows() {
   echo "t show access​-ke​y-windows was typed"
 }
 #### t show status managers|msrs|workers|windows|all  
+t-show-status() {
+  case "$1" in 
+  "managers") t-show-status-managers
+              exit;;
+  "msrs") t-show-status-msrs
+          exit;;
+  "workers") t-show-status-workers 
+            exit;;
+  "windows") t-show-status-windows 
+            exit;;
+  "all") t-show-status-all
+        exit;;
+  *) echo "t show status managers|msrs|workers|windows|all"
+  esac 
+}
 t-show-status-managers() { 
   echo "t show status managers was typed"
 }
@@ -193,7 +225,7 @@ t-show-status-windows() {
   echo "t show status windows was typed"
 }
 t-show-status-all() {
-  t-show-status-msrs
+  t-show-status-managers 
   t-show-status-msrs
   t-show-status-workers
   # t-show-status-wins  # if windows VMs exist 
@@ -206,6 +238,27 @@ t-show-versions() {
 t-show-all() { 
   echo "t show all  was typed"
 }
+### t exec etcdctl
+t-exec-etcdctl() {
+  echo "t exec etcdctl was typed "
+}
+#### t exec rethinkcli mke|msr
+t-exec-rethinkcli() {
+  case "$1" in
+    "mke") t-exec-rethinkcli-mke
+	      exit;;
+	"msr") t-exec-rethinkcli-msr 
+	      exit;;
+	*) echo "t exec rethinkcli mke|msr"
+	  exit;;
+  esac
+}
+t-exec-rethinkcli-mke() {
+  echo "t exec rethinkcli mke was typed"
+}
+t-exec-rethinkcli-msr() {
+  echo "t exec rethinkcli msr was typed"
+}
 ##### 1st level usage function : 
 usage1() {
   echo "t deploy lab|cluster|instances "
@@ -213,22 +266,23 @@ usage1() {
   echo "t show versions|status|all "
   echo "t show ip|dns|creds ...."
   echo "t show status|hostname managers|workers|msrs|windows "
+  echo "t exec etcdctl "
+  echo "t exec rethinkcli ..."
   echo "  When you finish your work:"
-  echo "t stop managers|workers|msrs|windows|manager1|msr2|worker3 "
+  echo "t stop managers|workers|msrs|windows|manager1 "
   echo "t destroy lab|cluster "
 }
-######### Parsing starts here, t is $0 , and we an have $1 $2 , or $1 $2 $3 depending on if the $# == 2 or $# == 3  ########
-###### t should be an alias for t-commandline.bash  executable file  ######
-#### ATTENTION : Maybe we will need to use break;; and not exit;; #### 
+
+######### Parsing starts here, t is $0 , and we can have $1 $2 , or $1 $2 $3 ########
 if [ $# -eq 2 ]; then 
   case "$1" in 
-  "deploy") t-deploy $2    # t deploy lab|cluster|instances
+  "deploy") t-deploy "$2"    # t deploy lab|cluster|instances
           exit ;;
-  "destroy") t-destroy $2  # t destroy lab|cluster
+  "destroy") t-destroy "$2"  # t destroy lab|cluster
            exit ;;
-  "stop")  t-stop $2       # t stop managers|msrs|workers|windows
+  "stop")  t-stop "$2"       # t stop managers|msrs|workers|windows
          exit;;
-  "show") case "$3" in 
+  "show") case "$2" in 
 		    "versions") t-show-versions
 			             exit ;;
 		    "all") t-show-all 
@@ -237,34 +291,63 @@ if [ $# -eq 2 ]; then
 			        exit;;
 			"access​-ke​y-windows") t-show-access​_ke​y_windows
 			        exit;;
-			 *) echo "t show versions|all|access​-ke​y-linux|access​-ke​y-windows "
+			 *) echo "t show versions|all|access-key-linux|access-key-windows "
+			    echo "t show status managers|msrs|workers|windows|all "
+				exit;;
 		  esac 
          exit;;
-  *) echo "t deploy|destroy|stop|show ... "
+   "gen")  # t gen client-bundle|msr-login|swarm-service|k8s-service|msr-images
+         case "$2" in 
+		 "client-bundle") t-gen-client_bundle 
+		                 exit;;
+		 "msr-login") t-gen-msr_login 
+		              exit;;
+		 "swarm-service") t-gen-swarm_service
+		                 exit;;
+		 "k8s-service") t-gen-k8s_service
+		                exit;;
+		 "msr-images") t-gen-msr_images
+		               exit;;
+		  *) echo "t gen client-bundle|msr-login|swarm-service|k8s-service|msr-images"
+		 esac
+        exit;;  
+   "exec") # t exec etcdctl
+          case "$2" in 
+		  "etcdctl") t-exec-etcdctl  
+		             exit ;; 
+		   *) echo "t exec etcdctl|rethinkcli "
+		      exit ;;
+          esac		   
+          exit;;
+   *) echo "t deploy|destroy|stop|show|gen ... "
      exit ;;
   esac 
 elif [ $# -eq 3 ]; then 
   case "$1" in 
   "show") # t show ip|dns|hostname|creds|status ...
          case "$2" in 
-		 "ip") t-show-ip $3
+		 "ip") t-show-ip "$3"
 				exit;;
-		 "dns") t-show-dns $3
+		 "dns") t-show-dns "$3"
 				exit;;
-		 "hostname") t-show-hostname $3
+		 "hostname") t-show-hostname "$3"
 		        exit;;
-		 "creds")  t-show-creds $3		
+		 "creds")  t-show-creds "$3"
 				exit;;
-		 "status") t-show-status $3
+		 "status") t-show-status "$3"
 		        exit;;
 		  *) echo " t show ip|dns|hostname|creds|status ... "
 		     exit;;
 		 esac 
 	    exit;;
-#  "gen")   # t gen client-bundle|
-#         case "$3" in 
-#        exit;;  
-  *)  usage1
+  "exec") case "$2" in 
+          "rethinkcli") t-exec-rethinkcli "$3"
+		  exit;;
+		  *) echo "t exec rethinkcli mke|msr"
+		  exit;;
+		  esac 
+         exit;;
+   *) usage1
       exit;;
   esac
 else 
